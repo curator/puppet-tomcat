cadcaa616599c3fd1037ad5fedbaf1409c4aff0d
pnorvell <pnorvell@gettyimages.com>
2014-02-11 14:58:08 -0800

    Because I hate them so much

--- spec/unit/tomcat_spec.rb
+++ spec/unit/tomcat_spec.rb
@@ -4,13 +4,14 @@ describe 'tomcat', :type => :class do
   context "When on unsupported operating system family" do
     let :facts do
       {
-        :osfamily   =>  'GNU/Linux'
+        :osfamily   =>  'Gentoo'
       }
     end
 
     it 'should send an *unsupported* error' do
       expect { should compile }.to raise_error(Puppet::Error, /Unsupported/)
     end
-
   end
+
+
 end
