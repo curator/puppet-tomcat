a5a4158053dec0eb936aa67a753c054b6d2b6f69
pnorvell <pnorvell@gettyimages.com>
2014-02-12 10:31:23 -0800

    Remove extraneous inheritance

--- manifests/webapp.pp
+++ manifests/webapp.pp
@@ -1,14 +1,7 @@
 # Class: tomcat::webapp
 #
 #
-class tomcat::webapp inherits tomcat::params {
+class tomcat::webapp {
 
-  if $::osfamily == 'RedHat' or $::operatingsystem == 'amazon' {
-    notice('Yay! we can do stuff')
-  } elsif $::osfamily == 'debian' {
-    warning('Debian/Ubuntu support may be coming soon.  Feel free to add it.')
-  } else {
-    warning("Unsupported osfamily ${::osfamily}.")
-  }
 
 }
