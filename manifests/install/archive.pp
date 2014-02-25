13fd76068ec925cf29e90991eff7f60b36236c30
pnorvell <pnorvell@gettyimages.com>
2014-02-25 15:07:40 -0800

    Fix more resource ordering issues and pathing for the expanded tar ball

--- manifests/install/archive.pp
+++ manifests/install/archive.pp
@@ -5,21 +5,28 @@ class tomcat::install::archive (
   $package_name           =   'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.50/bin/apache-tomcat-7.0.50.tar.gz',
   $version                =   '7.0.50',
   $archive_download_dir   =   '/usr/local/src',
-  $archive_target_dir     =   undef,
+  $archive_target_dir     =   '/usr/share',
   $manage_user            =   true,
   $tomcat_user            =   'tomcat',
   $manage_group           =   true,
   $tomcat_group           =   'tomcat',
   $remove_default_apps    =   true,
   $remove_default_manager =   true,
+  $symlink_to_maj_version =   true
   ) {
 
-  # XXX Bleh way to default to /usr/share/tomcat${tomcat_maj_version} due to variable interpolation issues
   $tomcat_maj_version = regsubst($version, '^([0-9]+)[.].*$', '\1')
-  if ! $archive_target_dir {
-    $target_dir = "/usr/share/tomcat${tomcat_maj_version}"
-  } else {
-    $target_dir = $archive_target_dir
+  $tomcat_expanded_dir = inline_template('<%= File.basename(@package_name,".tar.gz") %>')
+  $tomcat_install_dir = "${archive_target_dir}/${tomcat_expanded_dir}"
+
+  archive { "apache-tomcat-${version}":
+    ensure           => present,
+    url              => $package_name,
+    follow_redirects => true,
+    extension        => 'tar.gz',
+    checksum         => false,
+    src_target       => $archive_download_dir,
+    target           => $archive_target_dir
   }
 
   if $manage_group {
@@ -37,70 +44,72 @@ class tomcat::install::archive (
         ensure  => present,
         gid     => $tomcat_group,
         system  => true,
-        before  => File[$target_dir]
+        before  => File[$tomcat_install_dir]
       }
     }
-  }
-
-  archive { "apache-tomcat-${version}":
-    ensure           => present,
-    url              => $package_name,
-    follow_redirects => true,
-    extension        => 'tar.gz',
-    checksum         => false,
-    src_target       => $archive_download_dir,
-    target           => $target_dir
   }
 
 
   # Remove the default tomcat apps?
   if $remove_default_apps {
     $default_apps = [
-      "${target_dir}/webapps/docs",
-      "${target_dir}/webapps/examples",
-      "${target_dir}/webapps/ROOT",
+      "${tomcat_install_dir}/webapps/docs",
+      "${tomcat_install_dir}/webapps/examples",
+      "${tomcat_install_dir}/webapps/ROOT",
     ]
     file { $default_apps:
       ensure  => absent,
       recurse => true,
       purge   => true,
       force   => true,
-      require => Archive["apache-tomcat-${version}"]
+      require => Archive["apache-tomcat-${version}"],
+      before  => File[$tomcat_install_dir]
     }
   }
 
   # Remove the default manager app?
   if $remove_default_manager {
     $manager_apps = [
-      "${target_dir}/webapps/manager",
-      "${target_dir}/webapps/host-manager"
+      "${tomcat_install_dir}/webapps/manager",
+      "${tomcat_install_dir}/webapps/host-manager"
     ]
     file { $manager_apps:
       ensure  => absent,
       recurse => true,
       purge   => true,
       force   => true,
-      require => Archive["apache-tomcat-${version}"]
+      require => Archive["apache-tomcat-${version}"],
+      before  => File[$tomcat_install_dir]
     }
   }
 
-  # Chown files in $target_dir
-  file { $target_dir:
+  # Chown files in $tomcat_install_dir
+  file { $tomcat_install_dir:
     ensure    => directory,
     owner     => $tomcat_user,
     group     => $tomcat_group,
     recurse   => true
   }
 
-  # Symlink to a moderately standard/
+  # Symlink to a moderately standard/location
   file { "/usr/bin/dtomcat${tomcat_maj_version}":
     ensure  => link,
-    target  => "${target_dir}/bin/catalina.sh"
+    target  => "${tomcat_install_dir}/bin/catalina.sh"
   }
 
+  if $symlink_to_maj_version {
+    file { "${archive_target_dir}/tomcat${tomcat_maj_version}":
+      ensure  => link,
+      target  => $tomcat_install_dir,
+      require => Archive["apache-tomcat-${version}"]
+    }
+  }
+
+  # Symlink to a
   # Relationships
   Group[$tomcat_group]                -> User[$tomcat_user]
   User[$tomcat_user]                  -> Archive["apache-tomcat-${version}"]
-  Archive["apache-tomcat-${version}"] -> File[$target_dir]
+  Archive["apache-tomcat-${version}"] -> File[$tomcat_install_dir]
+  Archive["apache-tomcat-${version}"] -> File["/usr/bin/dtomcat${tomcat_maj_version}"]
 }
 
--- spec/unit/tomcat_install_archive_spec.rb
+++ spec/unit/tomcat_install_archive_spec.rb
@@ -11,9 +11,10 @@ describe 'tomcat::install::archive', :type => :class do
       :tomcat_group           => 'conehead',
       :manage_user            => true,
       :tomcat_user            => 'conehead',
-      :archive_target_dir     => '/usr/share/tomcat7',
+      :archive_target_dir     => '/usr/share',
       :remove_default_apps    => true,
-      :remove_default_manager => true
+      :remove_default_manager => true,
+      :symlink_to_maj_version => true,
     }
   end
 
@@ -27,22 +28,26 @@ describe 'tomcat::install::archive', :type => :class do
   end
 
   it 'should chown the directories to the specified tomcat user' do
-    should contain_file('/usr/share/tomcat7')
+    should contain_file('/usr/share/apache-tomcat-7.0.50')
   end
 
   it 'should remove default apps, if remove_default_apps is true' do
-    should contain_file('/usr/share/tomcat7/webapps/docs')
-    should contain_file('/usr/share/tomcat7/webapps/examples')
-    should contain_file('/usr/share/tomcat7/webapps/ROOT')
+    should contain_file('/usr/share/apache-tomcat-7.0.50/webapps/docs')
+    should contain_file('/usr/share/apache-tomcat-7.0.50/webapps/examples')
+    should contain_file('/usr/share/apache-tomcat-7.0.50/webapps/ROOT')
   end
 
   it 'should remove manager, if remove_default_manager is false' do
-    should contain_file('/usr/share/tomcat7/webapps/manager')
-    should contain_file('/usr/share/tomcat7/webapps/host-manager')
+    should contain_file('/usr/share/apache-tomcat-7.0.50/webapps/manager')
+    should contain_file('/usr/share/apache-tomcat-7.0.50/webapps/host-manager')
   end
 
   it 'should create a user-space startup/shutdown symlink' do
     should contain_file('/usr/bin/dtomcat7')
+  end
+
+  it 'should create a symlink to major version director' do
+    should contain_file('/usr/share/tomcat7')
   end
 
 end
